ac93c89ed5bd618ff336f1943b0c9c435fa57e37
Ryan Su <suxi@oregonstate.edu>
2021-05-25 17:40:47 -0700

    Wrote Second Test

--- test_leapYear.py
+++ test_leapYear.py
@@ -5,5 +5,8 @@ class TestCase(unittest.TestCase):
     def testOne(self):
         self.assertEqual(leapYear.isLeapYear(400),True)
 
+    def testTwo(self):
+        self.assertEqual(leapYear.isLeapYear(100),False)
+
 if __name__ == '__main__':
     unittest.main()
